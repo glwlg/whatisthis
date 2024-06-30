eb9a66b6e4f643e7383f1a542c7f8f966ee2d86e
guoluwei <651971564@qq.com>
2024-06-30 15:48:03 +0800

    init

--- frontend/src/components/home.vue
+++ frontend/src/components/home.vue
@@ -1,45 +1,80 @@
 <template>
   <v-app>
-    <v-main style="--wails-draggable: drag">
-      <v-container>
-        <!-- 判断data中的字段是否有值，如果没有则显示表单 -->
-        <v-form v-if="showSetting" v-model="valid" ref="form" lazy-validation>
-          <v-text-field
-              v-model="openai.api_key"
-              :rules="apiKeyRules"
-              label="API Key"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="openai.base_url"
-              :rules="baseUrlRules"
-              label="Base URL"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="openai.model"
-              :rules="modelRules"
-              label="模型"
-              required
-          ></v-text-field>
-          <v-text-field
-              v-model="openai.api_version"
-              :rules="apiVersionRules"
-              label="API Version"
-              required
-          ></v-text-field>
-          <v-btn @click="submit">Submit</v-btn>
-        </v-form>
-        <div v-else>
-          <div v-if="data.resultText">
-            <div id="result" class="result">{{ data.resultText }}</div>
-            <v-btn @click="copy">copy</v-btn>
-          </div>
-        </div>
+    <v-main>
+      <v-container class="ma-0 pa-0">
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="6">
+            <v-card id="main-box" class="elevation-12" outlined>
+              <v-card-title v-if="showSetting"  style="--wails-draggable: drag">
+                <span class="headline">API 设置</span>
+              </v-card-title>
+              <v-card-title v-else  style="--wails-draggable: drag">
+                <span class="headline">这是什么?</span>
+              </v-card-title>
+              <v-card-text>
+                <v-form v-if="showSetting" v-model="valid" ref="form" lazy-validation>
+                  <v-text-field
+                      v-model="openai.api_key"
+                      :rules="apiKeyRules"
+                      label="API Key"
+                      required
+                      outlined
+                      dense
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="openai.base_url"
+                      :rules="baseUrlRules"
+                      label="Base URL"
+                      required
+                      outlined
+                      dense
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="openai.model"
+                      :rules="modelRules"
+                      label="模型"
+                      required
+                      outlined
+                      dense
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="openai.api_version"
+                      :rules="apiVersionRules"
+                      label="API Version"
+                      required
+                      outlined
+                      dense
+                  ></v-text-field>
+                  <v-btn color="primary" @click="submit" class="mr-4">Submit</v-btn>
+                </v-form>
+                <div v-else>
+                  <div v-if="data.resultText" class="pa-3">
+                    <div id="result" class="result ma-3" v-text="data.resultText"></div>
+                    <v-btn color="primary" @click="copy">Copy</v-btn>
+                  </div>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.result {
+  max-height: 80vh;
+  overflow-y: auto;
+  background-color: #f1f1f1;
+  border-radius: 8px;
+  padding: 10px;
+  font-size: 16px;
+}
+#main-box{
+  height: 100vh;
+}
+</style>
 
 <script setup>
 import {onMounted, reactive, ref} from 'vue';
